2e2067e904fc2cd2e915112edf15aba11f7931c8
Jan W. <JanWittke97@web.de>
2019-04-16 21:54:48 +0200

    2 Testcases: Eine falsche Sequenz und eine richtige.

--- src/Task1/SequenceCheckInterface.java
+++ src/Task1/SequenceCheckInterface.java
@@ -1,6 +1,10 @@
 package Task1;
 
 public interface SequenceCheckInterface {
+	
 	void setDependencies(String[][] dependencies);
+	
+	String[][] getDependencies();
+	
 	boolean isWellSorted(String[] sequence);
 }
